728786ae79a601a8a83b2b36175fc7bb3fb5a493
sepehr <sepehrgh01@gmail.com>
2019-12-22 16:55:32 +0330

    Added default response for BasicAzeronResponseDto

--- src/main/java/io/pinect/azeron/server/domain/dto/BasicAzeronReponseDto.java
+++ src/main/java/io/pinect/azeron/server/domain/dto/BasicAzeronReponseDto.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class BasicAzeronReponseDto {
-    private ResponseStatus status;
+    private ResponseStatus status = ResponseStatus.OK;
 }
